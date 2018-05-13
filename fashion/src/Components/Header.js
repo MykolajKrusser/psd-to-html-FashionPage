c26e06f8021b5b51de7949d2d5e009c18938752e
Nicolas <nicolaskrusser@gmail.com>
2018-05-13 21:05:34 +0200

    added some styles to nav block

--- fashion/src/Components/Header.js
+++ fashion/src/Components/Header.js
@@ -4,13 +4,21 @@ import './Header.css';
 class Header extends Component {
   render() {
     return (
-        <header className="header flex content-wr">
-          <div className="menuBtn">a</div>
+        <header className="header flex">
+          <div className="menuBtns">
+            <div className="menuBtnsWrapper flex">
+              <a href="#" className="menuBtn"></a>
+              <a href="#" className="menuBtn menuBtnActive"></a>
+              <a href="#" className="menuBtn"></a>
+              <a href="#" className="menuBtn"></a>
+              <a href="#" className="menuBtn"></a>
+            </div>
+          </div>
           <div className="logo">W/Fashion</div>
           <nav className="nav flex">
             <a href="#">New</a>
             <a href="#">Sale</a>
-            <a href="#">My Cart</a>
+            <a href="#"><span className="myCards">2</span> My Cart</a>
             <a href="#">Login/Register</a>
           </nav>
         </header>
