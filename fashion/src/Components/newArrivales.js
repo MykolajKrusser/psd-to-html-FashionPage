a80eee2e3e310a7756fdce09910c4f1fb0c6b98f
Nicolas <nicolaskrusser@gmail.com>
2018-06-12 21:32:59 +0200

    add local json file

--- fashion/src/Components/newArrivales.js
+++ fashion/src/Components/newArrivales.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import './Newarrivales.css';
+import data from '../database/NewProducts';
 
 class Newarrivales extends Component {
+  constructor(){
+    super();
+    this.state = {
+        products: []
+    }
+  }
+  getProd(){
+    this.setState({products: data})
+  }
+  componentWillMount(){
+    this.getProd()
+  }
+  componentDidMount(){
+    this.getProd()
+  }
   render() {
+    console.log(this.state.products)
     return (
       <section id="newArrivels_prod" className="content-wr flex">
           <div className="newArrivales_prod col"></div>
