cc9ff7ccbe0bea9a743d4466cdf09897f753b01e
Nicolas <nicolaskrusser@gmail.com>
2018-06-21 19:39:12 +0200

    add some styles

--- fashion/src/Components/NewProduct/NewProduct.js
+++ fashion/src/Components/NewProduct/NewProduct.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 class NewProduct extends Component {
   render() {
     return (
-      <div>
+      <div className="newArrivales_prod col">
           <img
           key={this.props.key}
           src={this.props.img}
-          className="newArrivales_prod col"
           />
+          <div className="newArrivales_prod_forhover">
+            <h3 className="newArrivales_prod_title">{this.props.title}</h3>
+            <div className="newArrivales_prod_price">{this.props.cost}</div>
+            <a href="#" className="newArrivales_prod_btn">Show now</a>
+          </div>
       </div>
     );
   }
