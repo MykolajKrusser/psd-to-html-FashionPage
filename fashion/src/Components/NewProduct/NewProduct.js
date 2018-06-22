bddb1fd7c13859de2916fa7e9a0c12e45501bcaa
Nicolas <nicolaskrusser@gmail.com>
2018-06-22 21:04:03 +0200

    work with styles

--- fashion/src/Components/NewProduct/NewProduct.js
+++ fashion/src/Components/NewProduct/NewProduct.js
@@ -10,7 +10,7 @@ class NewProduct extends Component {
           />
           <div className="newArrivales_prod_forhover">
             <h3 className="newArrivales_prod_title">{this.props.title}</h3>
-            <div className="newArrivales_prod_price">{this.props.cost}</div>
+            <div className="newArrivales_prod_price">{this.props.cost+"$"}</div>
             <a href="#" className="newArrivales_prod_btn">Show now</a>
           </div>
       </div>
